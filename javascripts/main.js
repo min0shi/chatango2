35ae9902b3ff94a15065c11f7937df44869656d9
min0shi <jmangafan@gmail.com>
2014-09-01 01:53:38 -0500

    removed rgb to hex function added browser dependent slimScroll behaviour (because of Firefox)

--- javascripts/main.js
+++ javascripts/main.js
@@ -89,25 +89,18 @@
 		});
 		type_id="";
 	});
-	
+
+	var step=20;
+	if(navigator.userAgent.match(/Firefox/)!=null){
+		step=1;
+	}
+	console.log(step);
 	$('.content').slimScroll({
 		height: '500px',
-		width: '720px'
+		width: '720px',
+		wheelStep : step
 	});
 	$('.slimScrollDiv').css(
 		"margin", "10px auto"
 	);
 });
-
-var hexDigits = new Array
-        ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
-
-//Function to convert hex format to a rgb color
-function rgb2hex(rgb) {
-	rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-	return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-}
-
-function hex(x) {
-	return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-}
